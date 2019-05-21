81fa216fe9a12aada4f526d237cc0c5329b11ca5
Roman Zimmermann <roman@more-onion.com>
2019-05-21 20:52:09 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Python package metadata for installing the auth-wsgi-middleware as a dependency."""
+
+from setuptools import setup
+
+setup(
+    name='impact-stack-auth-wsgi-middleware',
+    version='0.0.1',
+    packages=['impact_stack.auth_wsgi_middleware'],
+    url='https://gitlab.more-onion.com/impact-stack/auth-wsgi-middleware',
+    maintainer='Roman Zimmermann',
+    maintainer_email='roman@more-onion.com',
+    install_requires=[
+        'itsdangerous',
+        'redis',
+        'werkzeug',
+    ]
+)
